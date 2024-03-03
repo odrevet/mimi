304d6a76ccc4a8a634c5551bac45009249041bd2
odrevet <odrevet@gmail.com>
2024-03-03 07:50:41 +0100

    info.sh print all info as json

--- var/www/info.sh
+++ var/www/info.sh
@@ -1,5 +1,24 @@
-info=$(mocp -Q "%state %file")
+#!/bin/bash
+
+tojson() {
+    declare -n v=$1
+    printf '%s\0' "${!v[@]}" "${v[@]}" |
+    jq -Rs 'split("\u0000") | . as $v | (length / 2) as $n | reduce range($n) as $idx ({}; .[$v[$idx]]=$v[$idx+$n])'
+}
+
+declare -A info_array
+
+while IFS= read -r line; do
+    key="${line%%:*}"
+    value="${line#*: }"
+
+    if [ -z "$value" ]; then
+        value="null"
+    fi
+
+    info_array["$key"]="$value"
+done <<< $(mocp -i)
 
 echo "Content-type: application/json"
 echo ""
-jq -n --arg info "$info" '{"info": $info}'
+tojson info_array
