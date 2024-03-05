88fb38dca0aab4ade23a131d2ecc501ea78aabce
odrevet <odrevet@gmail.com>
2024-03-05 22:02:06 +0100

    return playlist on play

--- server/var/www/play.sh
+++ server/var/www/play.sh
@@ -1,8 +1,18 @@
 source decode-params.sh
 filter=${params["filter"]}
+[[ " ${params[@]} " =~ " shuffle " ]] && shuffle="--shuffle" || shuffle=""
 
-bash /home/player/playlist.sh --filter "$filter" --truncat 32
+bash /home/player/playlist.sh --filter ${params["filter"]} --truncat 32 $shuffle
+
+# Read playlist
+entries=()
+while IFS= read -r line; do
+    if [[ "$line" == \#* ]]; then
+        continue  # Ignore commented lines
+    fi
+    entries+=("$line")
+done < "/home/player/.moc/playlist.m3u"
 
 echo "Content-type: application/json"
 echo ""
-jq -n --arg filter "$filter" '{"filter": $filter}'
+jq -c -n '$ARGS.positional' --args "${entries[@]}"
